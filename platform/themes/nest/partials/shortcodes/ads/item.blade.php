ee91ae3d8f099ceb6c615a30558cb54657c0f454
PhanPhan62 <nguyenyen69000@gmail.com>
2024-05-31 03:56:30 +0000

    Giới hạn ký tự hiển thị SHOPNOW

--- platform/themes/nest/partials/shortcodes/ads/item.blade.php
+++ platform/themes/nest/partials/shortcodes/ads/item.blade.php
@@ -1,22 +1,23 @@
-<div class="col-12 banner-img wow animate__animated animate__fadeInUp {{ $class ?? '' }}" @if (!empty($loop)) data-wow-delay="{{ $loop->iteration * 2 / 10 }}" @endif>
+<div class="col-12 banner-img wow animate__animated animate__fadeInUp {{ $class ?? '' }}"
+    @if (!empty($loop)) data-wow-delay="{{ ($loop->iteration * 2) / 10 }}" @endif>
     {!! AdsManager::displayAds($ads->key) !!}
     <div class="banner-text">
         <!-- <h4>{!! BaseHelper::clean(nl2br($ads->getMetaData('subtitle', true) ?: '')) !!}</h4>
         @if ($buttonText = $ads->getMetaData('button_text', true))
-            <a href="{{ route('public.ads-click', $ads->key) }}" class="btn btn-xs">
+<a href="{{ route('public.ads-click', $ads->key) }}" class="btn btn-xs">
                 {{ $buttonText }} <i class="fi-rs-arrow-small-right"></i>
             </a>
-        @endif -->
-<!-- code tự viết -->
+@endif -->
+        <!-- code tự viết -->
         @php
             use Illuminate\Support\Str;
             $subtitle = $ads->getMetaData('subtitle', true) ?: '';
             $cleanedSubtitle = BaseHelper::clean(nl2br($subtitle));
-            $limitedSubtitle = Str::limit($cleanedSubtitle,35, '...');
+            $limitedSubtitle = Str::limit($cleanedSubtitle, 35, '...');
             $buttonText = $ads->getMetaData('button_text', true);
         @endphp
 
-        @if($limitedSubtitle)
+        @if ($limitedSubtitle)
             <h4>{!! $limitedSubtitle !!}</h4>
         @endif
 
